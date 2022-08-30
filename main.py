a8d87f8523b667e571a66612a35943152e3da944
Kyrylo Vdovin <artegerartegerasd@gmail.com>
2022-08-30 19:59:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+# import
+
+# def build_dataless_neural_network():
+#     def input_layer =
+#
+#     pass
+
+
+def build_test_graph():
+    return np.array(
+        [0, 1, 1, 0, 0, 0, 1, 0, ],
+        [1, 0, 0, 1, 0, 0, 0, 1, ],
+        [1, 0, 0, 1, 1, 0, 0, 0, ],
+        [0, 1, 1, 0, 0, 1, 0, 0, ],
+        [0, 0, 1, 0, 0, 1, 1, 0, ],
+        [0, 0, 0, 1, 1, 0, 0, 1, ],
+        [1, 0, 0, 0, 1, 0, 0, 1, ],
+        [0, 1, 0, 0, 0, 1, 1, 0, ],
+    )
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
